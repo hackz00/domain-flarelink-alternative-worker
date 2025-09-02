1d4fef49050d678837dd91e80d8da5b79b82e4b0
nohackzdontcheck <120279282+hackz00@users.noreply.github.com>
2025-09-02 18:21:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
     const subdomain = url.hostname.split(".")[0];
-    
+
     const choice = await env.FLARELINK_KV.get(subdomain);
     if (choice) {
       return Response.redirect(choice, 302);
     }
 
-    const target = `http://172.96.141.252`; 
+    const target = "http://172.96.141.252"; 
     await env.FLARELINK_KV.put(subdomain, target);
     return Response.redirect(target, 302);
   }
