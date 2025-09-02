2108c3a3d2268155cdcc9d63ab7afd527983937e
nohackzdontcheck <120279282+hackz00@users.noreply.github.com>
2025-09-02 18:13:21 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+export default {
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+    const host = url.hostname;
+    const subdomain = host.split(".")[0]; 
+
+    const choice = await env.FLARELINK_KV.get(subdomain);
+    if (choice) {
+      return Response.redirect(choice, 302); 
+    }
+
+
+    const target = `http://172.96.141.252`;
+    await env.FLARELINK_KV.put(subdomain, target);
+    return Response.redirect(target, 302);
+  }
+}
